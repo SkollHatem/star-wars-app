a633b16989fe02c4e687f613059cf92bfa3f9b73
Emmanuel Villegas <emmanuelvillegasgonzalez@gmail.com>
2023-07-27 08:50:46 -0400

    add test e2e with cypress

---
+++ cypress/e2e/People.cy.ts
@@ -0,0 +1,19 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.viewport(360, 740);
+    cy.visit("/");
+    // cy.get("input").clear().type("Venezuela")
+    cy.contains("people").click();
+    cy.wait(10000);
+    cy.get("#logo").click();
+    cy.wait(2000);
+    cy.contains("people").click();
+    cy.scrollTo(0, 1000);
+    cy.get("#next").click();
+    cy.scrollTo(0, 0);
+    cy.wait(2000);
+    cy.get("#prev").click();
+    cy.scrollTo(0, 0);
+    cy.get("#people-1").click();
+  });
+});
