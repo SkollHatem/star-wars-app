0156b9bd007877fdbb751d549651ee5eacc4b725
Emmanuel Villegas <emmanuelvillegasgonzalez@gmail.com>
2023-07-27 09:30:24 -0400

    fix

--- cypress/e2e/People.cy.ts
+++ cypress/e2e/People.cy.ts
@@ -3,14 +3,14 @@ describe("template spec", () => {
     cy.viewport(360, 740);
     cy.visit("/");
     cy.contains("people").click();
-    cy.wait(10000);
+    cy.wait(20000);
     cy.get("#logo").click();
-    cy.wait(2000);
+    cy.wait(20000);
     cy.contains("people").click();
     // cy.scrollTo(0, 1000);
     cy.get("#next").click();
     // cy.scrollTo(0, 0);
-    cy.wait(2000);
+    cy.wait(20000);
     cy.get("#prev").click();
     // cy.scrollTo(0, 0);
     cy.get("#people-1").click();
